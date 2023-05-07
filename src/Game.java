8f4cdd7e6e05813c6b05b6fb7bb84b4103e6b54d
Maddox <rynvnskl@yahoo.com>
2023-05-06 22:14:28 -0400

    [BROKEN] May need to fix sprite flickering still, need to make seperate timer from development.

--- src/Game.java
+++ src/Game.java
@@ -3,30 +3,64 @@ import Utils.*;
 
 public class Game {
     private final Window window;
+    private final Keyboard keyboard;
     private final Sprite background;
     private Snake[] snake = new Snake[1]; 
+    private boolean right, left, up, down;
 
     public Game() {
         window = new Window();
+        keyboard = new Keyboard();
         background = new Sprite("Background.png", 750, 750, 0, 0);
-        window.add(background.get());
         configureWindow(window);
-
+        window.add(keyboard);
+        
         snake[0] = new Snake(7, 7, Snake.BodyType.HEAD);
     }
-
+    
     public void Update() {
         for (Snake bodyPart:snake) bodyPart.Update();
-    }
 
+        if (keyboard.wasKeyPressed("left") && !right) {
+            left = true;
+            right = false;
+            up = false;
+            down = false;
+        }
+        if (keyboard.wasKeyPressed("right") && !left) {
+            left = false;
+            right = true;
+            up = false;
+            down = false;
+        }
+        if (keyboard.wasKeyPressed("up") && !down) {
+            left = false;
+            right = false;
+            up = true;
+            down = false;
+        }
+        if (keyboard.wasKeyPressed("down") && !up) {
+            left = false;
+            right = false;
+            up = false;
+            down = true;
+        }
+
+        if (left) snake[0].setPosition((int) snake[0].getPosition().getX() - 50, (int) snake[0].getPosition().getY()); 
+        else if (right) snake[0].setPosition((int) snake[0].getPosition().getX() + 50, (int) snake[0].getPosition().getY()); 
+        else if (up) snake[0].setPosition((int) snake[0].getPosition().getX(), (int) snake[0].getPosition().getY() - 50); 
+        else if (down) snake[0].setPosition((int) snake[0].getPosition().getX(), (int) snake[0].getPosition().getY() + 50); 
+    }
+    
     public void Draw() {
         window.draw();
         for (Snake bodyPart:snake) window.add(bodyPart.get());
+        window.add(background.get());
     }
 
     public void configureWindow(Window window) {
         var config = new WindowConfiguration();
-        config.setTitle("Example Window");
+        config.setTitle("Snake");
         config.setSize(750, 750);
         config.excludeInsets(true);
         config.setResizable(false);
--- src/Snake.java
+++ src/Snake.java
@@ -8,14 +8,12 @@ public class Snake {
     private BodyType bodyType;
     private Sprite bodyTexture;
     private final Grid grid;
-    private int x;
-    private int y;
 
     public Snake(int x, int y, BodyType bodyType) {
         this.grid = new Grid(50, 50);
         this.bodyType = bodyType;
-        this.x = (int) grid.get(x, y, Translate.FROM_GRID).getX();
-        this.y = (int) grid.get(x, y, Translate.FROM_GRID).getY();
+        x = (int) grid.get(x, y, Translate.FROM_GRID).getX();
+        y = (int) grid.get(x, y, Translate.FROM_GRID).getY();
         bodyTexture = new Sprite("Square.png", 50, 50, x, y);
     }
 
@@ -27,7 +25,15 @@ public class Snake {
     }
 
     public void Update() {
+        // System.out.println(x + " | " + y);
+    }
 
+    public void setPosition(int x, int y) { 
+        bodyTexture.setPosition(x, y);
+    }
+
+    public Vector2D getPosition() { 
+        return bodyTexture.getPosition(); 
     }
 
     public JLabel get() {
